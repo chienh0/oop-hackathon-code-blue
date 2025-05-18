77325210f828f2cfd820f4c05a8a796ac5cbdced
Chien Ho <chienho@Chiens-MBP-2.localdomain>
2025-05-18 14:03:36 -0700

    update fuzzy matching

--- streaming_recording.py
+++ streaming_recording.py
@@ -71,14 +71,18 @@ cpr_start_phrases = [
 	"chest compressions initiated",
 	"i'm on compressions",
 	"initiating compressions",
+	"cpr initiated",
+    "starting cpr",
+	"start cpr",
 	"cpr started",
+	"cpr start",
 	"starting compressions",
 	"begin cpr now",
 	"compressions going",
 	"starting cycles now"
 ]
 
-FUZZY_THRESHOLD = 90  # Adjust as needed for sensitivity
+FUZZY_THRESHOLD = 70  # Adjust as needed for sensitivity
 
 def cancel_cpr_timer():
 	global cpr_timer_task
@@ -102,8 +106,8 @@ async def cpr_timer(triggered_by_phrase=None):
 	for remaining in range(total_seconds, 0, -1):
 		mins, secs = divmod(remaining, 60)
 		timer_area.markdown(f"## ⏳ CPR Timer: {mins:02d}:{secs:02d}")
-		if remaining == 100 and triggered_by_phrase:
-			speak_text_streamlit("20 seconds until next pulse check.")
+		if remaining == 110 and triggered_by_phrase:
+			speak_text_streamlit("10 seconds until next pulse check.")
 		await asyncio.sleep(1)
 	timer_area.markdown("## ⏰ 2 minutes up! Time for pulse check.")
 	del st.session_state['cpr_timer_display']
@@ -207,7 +211,7 @@ def speak_text_streamlit(text):
 	fp.seek(0)
 	st.audio(fp, format='audio/mp3')
 
-st.title('Code Blue')
+st.title('Code Blue Co-pilot')
 
 # Configuration sidebar
 # with st.sidebar:
